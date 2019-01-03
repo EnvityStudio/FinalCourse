592be78751e6f730bdaeafc7fc058dbd0d8f784f
ThuanNguyen <thuanit.mta@gmail.com>
2019-01-03 23:35:03 +0700

    dont know

--- app/src/main/java/com/example/admin/finishcourse/lessonEight/Constants.java
+++ app/src/main/java/com/example/admin/finishcourse/lessonEight/Constants.java
@@ -5,6 +5,7 @@ package com.example.admin.finishcourse.lessonEight;
  * @datetime 2018/12/26
  */
 public class Constants {
-    public static final String LOCALHOST = "http://10.0.3.2:3000";
+    //    public static final String LOCALHOST = "http://10.0.3.2:3000";
+    public static final String LOCALHOST = "http://192.168.1.12:3000"; //json-server --watch student.json --host 192.168.1.12
     public static final String PATH_STUDENT = "/studentList";
 }
